caec468c9d4bf543e949866529bfcb0fab209eeb
Mario Vitor Fernandes <mvitor.f@gmail.com>
2022-12-01 20:55:55 -0300

    rounded pieces

--- lib/src/puzzle_piece/puzzle_piece_clipper.dart
+++ lib/src/puzzle_piece/puzzle_piece_clipper.dart
@@ -76,8 +76,13 @@ Path getPiecePath(
 
   if (posSide.top != 0) {
     path.extendWithPath(
-      calculatePoint(Axis.horizontal, topLeft.dy,
-          Offset(offsetCenter.dx, topMiddle), radiusPoint),
+      calculatePoint(
+          axis: Axis.horizontal,
+          fromPoint: topLeft.dy,
+          point: Offset(offsetCenter.dx, topMiddle),
+          radiusPoint: radiusPoint,
+          isLeft: false,
+          isTop: true),
       Offset.zero,
     );
   }
@@ -85,24 +90,39 @@ Path getPiecePath(
 
   if (posSide.right != 0) {
     path.extendWithPath(
-        calculatePoint(Axis.vertical, topRight.dx,
-            Offset(rightMiddle, offsetCenter.dy), radiusPoint),
+        calculatePoint(
+            axis: Axis.vertical,
+            fromPoint: topRight.dx,
+            point: Offset(rightMiddle, offsetCenter.dy),
+            radiusPoint: radiusPoint,
+            isLeft: false,
+            isTop: false),
         Offset.zero);
   }
   path.lineTo(bottomRight.dx, bottomRight.dy);
 
   if (posSide.bottom != 0) {
     path.extendWithPath(
-        calculatePoint(Axis.horizontal, bottomRight.dy,
-            Offset(offsetCenter.dx, bottomMiddle), -radiusPoint),
+        calculatePoint(
+            axis: Axis.horizontal,
+            fromPoint: bottomRight.dy,
+            point: Offset(offsetCenter.dx, bottomMiddle),
+            radiusPoint: -radiusPoint,
+            isLeft: false,
+            isTop: false),
         Offset.zero);
   }
   path.lineTo(bottomLeft.dx, bottomLeft.dy);
 
   if (posSide.left != 0) {
     path.extendWithPath(
-        calculatePoint(Axis.vertical, bottomLeft.dx,
-            Offset(leftMiddle, offsetCenter.dy), -radiusPoint),
+        calculatePoint(
+            axis: Axis.vertical,
+            fromPoint: bottomLeft.dx,
+            point: Offset(leftMiddle, offsetCenter.dy),
+            radiusPoint: -radiusPoint,
+            isLeft: true,
+            isTop: false),
         Offset.zero);
   }
   path.lineTo(topLeft.dx, topLeft.dy);
@@ -112,24 +132,81 @@ Path getPiecePath(
   return path;
 }
 
-Path calculatePoint(
-  Axis axis,
-  double fromPoint,
-  Offset point,
-  double radiusPoint,
-) {
+Path calculatePoint({
+  required Axis axis,
+  required double fromPoint,
+  required Offset point,
+  required double radiusPoint,
+  required bool isLeft,
+  required bool isTop,
+}) {
   final Path path = Path();
+
+  const extremeEdgeFactor = 3.5;
 
   if (axis == Axis.horizontal) {
     path.moveTo(point.dx - radiusPoint / 2, fromPoint);
-    path.lineTo(point.dx - radiusPoint / 2, point.dy);
+    path.quadraticBezierTo(
+      point.dx - radiusPoint,
+      (fromPoint + (3 * point.dy)) / 4,
+      point.dx - radiusPoint / 2,
+      point.dy,
+    );
+    // path.lineTo(point.dx - radiusPoint / 2, point.dy);
+    path.quadraticBezierTo(
+      point.dx,
+      point.dy < fromPoint
+          ? (!isTop
+              ? point.dy + radiusPoint / extremeEdgeFactor
+              : point.dy - radiusPoint / extremeEdgeFactor)
+          : (isTop
+              ? point.dy + radiusPoint / extremeEdgeFactor
+              : point.dy - radiusPoint / extremeEdgeFactor),
+      point.dx + radiusPoint / 2,
+      point.dy,
+    );
     path.lineTo(point.dx + radiusPoint / 2, point.dy);
-    path.lineTo(point.dx + radiusPoint / 2, fromPoint);
+
+    path.quadraticBezierTo(
+      point.dx + radiusPoint,
+      (fromPoint + (3 * point.dy)) / 4,
+      point.dx + radiusPoint / 2,
+      fromPoint,
+    );
+    //path.lineTo(point.dx + radiusPoint / 2, fromPoint);
   } else if (axis == Axis.vertical) {
     path.moveTo(fromPoint, point.dy - radiusPoint / 2);
-    path.lineTo(point.dx, point.dy - radiusPoint / 2);
-    path.lineTo(point.dx, point.dy + radiusPoint / 2);
-    path.lineTo(fromPoint, point.dy + radiusPoint / 2);
+
+    ///Previous original
+    // path.lineTo(point.dx, point.dy - radiusPoint / 2);
+// path.lineTo(point.dx, point.dy + radiusPoint / 2);
+    // path.lineTo(fromPoint, point.dy + radiusPoint / 2);
+    path.quadraticBezierTo(
+      (fromPoint + (3 * point.dx)) / 4,
+      point.dy - radiusPoint,
+      point.dx,
+      point.dy - radiusPoint / 2,
+    );
+
+    path.quadraticBezierTo(
+      point.dx < fromPoint
+          ? (isLeft
+              ? point.dx + radiusPoint / extremeEdgeFactor
+              : point.dx - radiusPoint / extremeEdgeFactor)
+          : (!isLeft
+              ? point.dx + radiusPoint / extremeEdgeFactor
+              : point.dx - radiusPoint / extremeEdgeFactor),
+      point.dy,
+      point.dx,
+      point.dy + radiusPoint / 2,
+    );
+
+    path.quadraticBezierTo(
+      (fromPoint + (3 * point.dx)) / 4,
+      point.dy + radiusPoint,
+      fromPoint,
+      point.dy + radiusPoint / 2,
+    );
   }
 
   return path;
